e11c01c071dce8cf070b7a1917d4bca83d4a4e49
dovahk11m <choongechobiz@gmail.com>
2025-09-03 12:25:54 +0900

    임포트 오류 수정 (조충희)

--- src/main/java/com/market/market_place/members/repositories/MemberAddressRepository.java
+++ src/main/java/com/market/market_place/members/repositories/MemberAddressRepository.java
@@ -1,5 +1,6 @@
 package com.market.market_place.members.repositories;
 
+import com.market.market_place.members.domain.MemberAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberAddressRepository extends JpaRepository<MemberAddress, Long> {
