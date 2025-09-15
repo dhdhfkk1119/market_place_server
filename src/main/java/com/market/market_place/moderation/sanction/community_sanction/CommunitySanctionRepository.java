843c873b84af59f39412332aa6ed3b0816528bda
jibaek <tngus000777@gmail.com>
2025-09-15 11:15:17 +0900

    오류해결

--- src/main/java/com/market/market_place/moderation/sanction/community_sanction/CommunitySanctionRepository.java
+++ src/main/java/com/market/market_place/moderation/sanction/community_sanction/CommunitySanctionRepository.java
@@ -1,6 +1,8 @@
 package com.market.market_place.moderation.sanction.community_sanction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommunitySanctionRepository extends JpaRepository<CommunitySanction,Long> {
 }
