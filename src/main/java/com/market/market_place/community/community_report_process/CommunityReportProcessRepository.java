eb14271eca34a573778ba0b23e52992d4bf1984f
sonjiyoon12 <1wldbs1908@gmail.com>
2025-09-22 09:02:34 +0900

    commnuityreportprocess

--- src/main/java/com/market/market_place/community/community_report_process/CommunityReportProcessRepository.java
+++ src/main/java/com/market/market_place/community/community_report_process/CommunityReportProcessRepository.java
@@ -3,10 +3,10 @@ package com.market.market_place.community.community_report_process;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommunityReportProcessRepository extends JpaRepository<CommunityReportProcess, Long> {
-    List<CommunityReportProcess> findAllByReportId(Long reportId);
-    Page<CommunityReportProcess> findAll(Pageable pageable);
+
+    @Query("select p from CommunityReportProcess p join fetch p.report r join fetch r.post")
+    Page<CommunityReportProcess> findAllWithPost(Pageable pageable);
 }
