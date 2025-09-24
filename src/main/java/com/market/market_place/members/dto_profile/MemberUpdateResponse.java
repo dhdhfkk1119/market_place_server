230ea8c0133ddc25daf89fbc990d6ff30cdb896f
dovahk11m <choongechobiz@gmail.com>
2025-09-24 16:57:49 +0900

    프로필 조회/수정시, 서버에서 멤버 role status 내려주도록 수정 (조충희)

--- src/main/java/com/market/market_place/members/dto_profile/MemberUpdateResponse.java
+++ src/main/java/com/market/market_place/members/dto_profile/MemberUpdateResponse.java
@@ -3,15 +3,19 @@ package com.market.market_place.members.dto_profile;
 import com.market.market_place.members.domain.Member;
 import com.market.market_place.members.domain.MemberProfile;
 import com.market.market_place.members.domain.MemberStatus;
+import com.market.market_place.members.domain.Role;
+import com.market.market_place.members.domain.Provider;
 import lombok.Getter;
 
 @Getter
 public class MemberUpdateResponse {
 
     private final Long id;
-    private final String name;
+    private final Role role;
+    private final Provider provider;
     private final MemberStatus status;
-    private final String profileImageBase64; // 프로필 이미지 필드 추가
+    private final String name;
+    private final String profileImageBase64;
 
     /**
      * 수정된 Member 엔티티를 MemberUpdateResponse DTO로 변환합니다.
@@ -19,12 +23,14 @@ public class MemberUpdateResponse {
      */
     public MemberUpdateResponse(Member member) {
         this.id = member.getId();
+        this.role = member.getRole();
+        this.provider = member.getProvider();
         this.status = member.getStatus();
 
         MemberProfile profile = member.getMemberProfile();
         if (profile != null) {
             this.name = profile.getName();
-            this.profileImageBase64 = profile.getProfileImageBase64(); // 프로필에서 Base64 이미지 가져오기
+            this.profileImageBase64 = profile.getProfileImageBase64();
         } else {
             this.name = null;
             this.profileImageBase64 = null;
